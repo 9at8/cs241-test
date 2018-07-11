fb8f94f8c20ae38eefae3680a8a89a9307800560
Aditya Thakral <aditya.1998thakral@gmail.com>
2018-07-10 22:48:01 -0400

    :boom: Use getopts instead of the hacky way

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USER="YourUsernameHere"
-SERVER="$USER@linux.student.cs.uwaterloo.ca"
+SERVER="linux.student.cs.uwaterloo.ca"
 
 INP=$(mktemp)
 WLP4I=$(mktemp)
@@ -9,14 +9,38 @@ MIPS=$(mktemp)
 
 cat /dev/stdin > $INP
 
-scp $INP "$SERVER:~/.run.tmp.wlp4" > /dev/null
+OPTIND=1
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+cmd="racket wlp4gen.rkt"
+mips_cmd="mips.twoints"
+assembly_file=$MIPS
+merls=""
+
+while getopts c:a:m: opt; do
+    case "$opt" in
+	c)  cmd=$OPTARG
+	    ;;
+	a)  assembly_file=$OPTARG
+            ;;
+	m)  mips_cmd=$OPTARG
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+merls=$(join_by " " $@)
+
+scp $INP "${USER}@${SERVER}:~/.run.tmp.wlp4" > /dev/null
 ssh $SERVER "./run.wlp4i.sh" > $WLP4I
 
-$1 < $WLP4I > $MIPS
+$cmd < $WLP4I > $assembly_file
 
-[ "$4" == "-i" ] && cat $MIPS
-
-scp $MIPS "$SERVER:~/.run.tmp.asm" > /dev/null
-ssh $SERVER "./run.mips.sh \"$2\" \"$3\""
+scp $assembly_file "${USER}@${SERVER}:~/.run.tmp.asm" > /dev/null
+ssh $SERVER ./run.mips.sh $mips_cmd \"$merls\"
 
 rm $INP $WLP4I $MIPS
